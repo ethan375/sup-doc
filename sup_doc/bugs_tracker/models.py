52464dd94861b2786604b50741e53f9b4ee675c4
ethan375 <ethanebel1@gmail.com>
2019-11-22 12:39:53 -0600

    setup bug model

--- sup_doc/bugs_tracker/models.py
+++ sup_doc/bugs_tracker/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Bug(models.Model):
+    title = models.CharField(max_length=100)
+    time_created = models.DateTimeField(timezone.now())
+    description = models.CharField(max_length=500)
+    submitters_name = models.CharField(max_length=100)
+    status = models.CharField(max_length=50)
+    assigned_dev = models.CharField(max_length=100)
+    completed_dev = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
