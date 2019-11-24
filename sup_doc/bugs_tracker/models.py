ea177d2180ef629874ef95dbfbce8f100773ab20
ethan375 <ethanebel1@gmail.com>
2019-11-24 17:30:13 -0600

    fixed model relationship bug

--- sup_doc/bugs_tracker/models.py
+++ sup_doc/bugs_tracker/models.py
@@ -9,7 +9,7 @@ class Ticket(models.Model):
     description = models.CharField(max_length=500)
     submitters_name = models.CharField(max_length=100)
     status = models.CharField(max_length=50)
-    assigned_dev = models.ForeignKey(BugUser, on_delete=models.CASCADE)
+    assigned_dev = models.ForeignKey('BugUser', on_delete=models.CASCADE)
     completed_dev = models.CharField(max_length=100)
 
     def __str__(self):
--- sup_doc/bugs_tracker/views.py
+++ sup_doc/bugs_tracker/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from . models import BugUser, Ticket
 
-# Create your views here.
+
+def index(request):
+    tickets = Ticket.objects.all()
+    context = {'tickets': tickets}
+
+    return render(request, 'index.html', context)
