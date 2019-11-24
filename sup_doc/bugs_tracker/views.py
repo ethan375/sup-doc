d7b04bd4531084f1f21947a595f97ad8eeb6dd3e
ethan375 <ethanebel1@gmail.com>
2019-11-24 17:42:43 -0600

    built out basic functions in the view file

--- sup_doc/bugs_tracker/views.py
+++ sup_doc/bugs_tracker/views.py
@@ -7,3 +7,40 @@ def index(request):
     context = {'tickets': tickets}
 
     return render(request, 'index.html', context)
+
+
+def new_ticket(request):
+    # need to import the form and instantiate and all that good stuff
+    if request.method == 'POST':
+        pass
+    else:
+        pass
+
+
+def edit_ticket(request, ticket_id):
+    # going to need to handle the form and the put/patch request, whatever it is going to be
+    pass
+
+
+def ticket_detail(request, ticket_id):
+    ticket = Ticket.objects.filter(id=ticket_id)
+    context = {'ticket', ticket}
+
+    return render(request, 'ticket/ticket_detail.html', context)
+
+
+def tickets_by_user(request, user_id):
+    user = BugUser.objects.filter(id=user_id)
+    context = {'user': user}
+
+    return render(request, 'ticket/ticket_by_user.html', context)
+
+
+def login_user(request):
+    # going to handle the fun of loggin in and all that shit 
+    pass
+
+
+def new_user(request):
+    # going to register new users and shit 
+    pass
