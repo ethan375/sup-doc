5e055965bcb298340e3acf975bf10b4829427cab
ethan375 <ethanebel1@gmail.com>
2019-11-24 08:15:43 -0600

    setup urls based on template files

--- sup_doc/bugs_tracker/urls.py
+++ sup_doc/bugs_tracker/urls.py
@@ -5,5 +5,9 @@ from . import views
 urlpatterns = [
     path('tickets/', views.index, name="index"),
     path('ticket/new', views.new_ticket, name="new_ticket"),
-    path('ticket/edit/<int:ticket_id>', views.edit_ticket, name="edit_ticket")
+    path('ticket/edit/<int:ticket_id>', views.edit_ticket, name="edit_ticket"),
+    path('ticket/<int:ticket_id>', views.ticket_detail, name="ticket_detail"),
+    path('user/<int:user_id>', views.tickets_by_user, name="tickets_by_user"),
+    path('user/login', views.login_user, name="user_login"),
+    path('user/new', views.new_user, name="new_user")
 ]
